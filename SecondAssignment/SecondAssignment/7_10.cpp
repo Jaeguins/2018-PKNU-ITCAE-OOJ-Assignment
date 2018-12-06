bc9d715a8983f27d420f348c18f371b428942ae2
Jaeguins <jsu4838@naver.com>
2018-12-06 13:16:34 +0900

    code shorten for assignment building

--- SecondAssignment/SecondAssignment/7_10.cpp
+++ SecondAssignment/SecondAssignment/7_10.cpp
@@ -16,28 +16,12 @@ ostream& operator<<(ostream& os, const CComplex& data) {
     cout << data.realNum << " + " << data.virtualNum<<"i";
     return os;
 }
-const CComplex CComplex::operator+(const CComplex& b) {
-    return CComplex(realNum + b.realNum, virtualNum + b.virtualNum);
-}
-const CComplex CComplex::operator-(const CComplex& b) {
-    return CComplex(realNum - b.realNum, virtualNum - b.virtualNum);
-}
-const CComplex CComplex::operator-(double b) {
-    return CComplex(realNum - b, virtualNum);
-}
-const CComplex CComplex::operator+(double b) {
-    return CComplex(realNum + b, virtualNum);
-}
-CComplex operator+(double b, CComplex& a) {
-    return a + b;
-}
-CComplex operator-(double b, CComplex& a) {
-    return a - b;
-}
-
-
-
-
+const CComplex CComplex::operator+(const CComplex& b) {return CComplex(realNum + b.realNum, virtualNum + b.virtualNum);}
+const CComplex CComplex::operator-(const CComplex& b) {return CComplex(realNum - b.realNum, virtualNum - b.virtualNum);}
+const CComplex CComplex::operator-(double b) {return CComplex(realNum - b, virtualNum);}
+const CComplex CComplex::operator+(double b) {return CComplex(realNum + b, virtualNum);}
+CComplex operator+(double b, CComplex& a) {return a + b;}
+CComplex operator-(double b, CComplex& a) {return a - b;}
 
 int main() {
     CComplex C1(1, 2), C2(3, 4);
@@ -46,8 +30,6 @@ int main() {
     CComplex C4 = C1 - C2;
     CComplex C5 = C1 - a;
     CComplex C6 = a - C1;
-
     cout << C3 << endl << C4 << endl << C5 << endl << C6 << endl;
-
     getchar();
 }
--- SecondAssignment/SecondAssignment/7_6.cpp
+++ SecondAssignment/SecondAssignment/7_6.cpp
@@ -4,52 +4,37 @@ using namespace std;
 class CArray {
 public:
     int data[5];
-    CArray() {
-        for (int i = 0; i < 5; i++)
-            data[i] = i;
-    }
-    void Print() {
-        for (int i = 0; i < 5; i++)
-            cout << data[i] << " ";
-        cout << endl;
-    }
-    CArray operator+(int notused) {
-        CArray t(*this);
-        int last = t.data[4];
-        for (int i = 4; i >= 1; i--) {
-            t.data[i] = t.data[i - 1];
-        }
-        t.data[0] = last;
-        return t;
-    }
-    CArray operator-(int notused) {
-        CArray t(*this);
-        int last = t.data[0];
-        for (int i = 0; i < 4; i--) {
-            t.data[i] = t.data[i + 1];
-        }
-        t.data[4] = last;
-        return t;
-    }
+    CArray() { for (int i = 0; i < 5; i++) data[i] = i; }
+    void Print() { for (int i = 0; i < 5; i++) cout << data[i] << " "; cout << endl; }
 };
-
+CArray operator+(CArray tk,int notused) {
+    CArray t(tk);
+    int last = t.data[4];
+    for (int i = 4; i >= 1; i--) t.data[i] = t.data[i - 1];
+    t.data[0] = last;
+    return t;
+}
+CArray operator-(CArray tk,int notused) {
+    CArray t(tk);
+    int last = t.data[0];
+    for (int i = 0; i < 4; i--) t.data[i] = t.data[i + 1];
+    t.data[4] = last;
+    return t;
+}
 CArray& operator+(CArray& ar) {
     int last = ar.data[4];
-    for (int i = 4; i >= 1; i--) {
-        ar.data[i] = ar.data[i - 1];
-    }
+    for (int i = 4; i >= 1; i--) ar.data[i] = ar.data[i - 1];
     ar.data[0] = last;
     return ar;
 }
 CArray& operator-(CArray& ar) {
     CArray t(ar);
     int last = ar.data[0];
-    for (int i = 0; i < 4; i++) {
-        ar.data[i] = ar.data[i + 1];
-    }
+    for (int i = 0; i < 4; i++) ar.data[i] = ar.data[i + 1];
     ar.data[4] = last;
     return ar;
 }
+//메인함수는 동일
 
 int main() {
     CArray ary1;
